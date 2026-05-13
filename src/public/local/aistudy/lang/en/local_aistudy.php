4b97e927473afe75270713c6980d6e4d52c24dcf
Nguyen Dang Cuong <143251480+nguyendangcuong201004@users.noreply.github.com>
2026-05-14 00:24:08 +0700

    Merge pull request #4 from nguyendangcuong201004/develop
    Enhance metrics, caching, and CI/CD workflow for deployment

---
+++ src/public/local/aistudy/lang/en/local_aistudy.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for local_aistudy.
+ *
+ * @package    local_aistudy
+ * @copyright  2026
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'AI Study integration';
+$string['aistudy:launch'] = 'Launch AI Study from a course';
+$string['launchaistudy'] = 'Study with AI Study';
+$string['baseurl'] = 'AI Study app URL';
+$string['baseurl_desc'] = 'Base URL of the AI Study application used for deep links from Moodle courses.';
+$string['openinnewtab'] = 'Open AI Study in a new tab';
+$string['openinnewtab_desc'] = 'If enabled, the launcher opens AI Study in a new browser tab.';
+$string['invalidbaseurl'] = 'The AI Study app URL is not configured correctly.';
+$string['privacy:metadata'] = 'The AI Study integration plugin does not store any personal data.';
